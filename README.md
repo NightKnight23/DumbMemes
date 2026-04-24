96a5826e275aea14543d16bb8c085ddd524cf6ce
Allen <allenjohn7990@gmail.com>
2026-04-24 21:17:44 +0530

    how

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
 # DumbMemes
 A project that uses webcam to detect facial expression and display dumb memes. 
-Inspiration and Reference - /reinesana/MeowCV
+Inspiration - /reinesana/MeowCV
 
 ## What i Plan 
-I plan to create a fun project to enjoy and learn coding. This project to aimed to have display meme's we often find ourself using in chat, and humour our expression. 
+I plan to create a fun project to enjoy and learn coding. This project is aimed to display meme's we often find ourself using in chat, and humour our expression. 
 
-I plan to display as many meme's as possible , which would require me to find the unique facial expression to trigger a specific meme. 
+I plan to display as meme's, which would require me to find the unique facial expression to trigger a specific meme. 
 
-NOTE : This project uses mediapipe.tasks library for facemesh instead of .solutions  
+NOTE : This project uses mediapipe.tasks library for facemesh instead of .solutions 
+
+# HOW IT WORKS
+
+The project will use your webcam in order to retrieve face and using mediapipe .tasks library will track your expressions.
+A expression satifying certain threshold will trigger a MEME as a corresponding responce, for instance: 
+   A WIDE OPEN EYES -> SHOCKED DOG
+   WIDE OPEN EYES + MOUTH -> SHOCKED MAN
+   MOUTH COVERED -> Jujutsu Kaisen FED UP EXPRESSION
+
+and such many....
 
 
 
--- main.py
+++ main.py
@@ -5,18 +5,25 @@ from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
 latest_face = None
+expression = 'None'
 
  #Callback 
 def result_callback(result, output_image,timestamp_ms):
-    global latest_face
-    if result.face_landmarks:
-        latest_face = result.face_landmarks[0]
+    global latest_face, expression
+    if not result.face_landmarks:
+        latest_face = None
+        expression = 'No Face Detected'
+        return
+    latest_face = result.face_landmarks[0]
+        
+        
         
 base_options = python.BaseOptions(model_asset_path='face_landmarker.task')
 options = vision.FaceLandmarkerOptions(
     base_options=base_options,
     running_mode=vision.RunningMode.LIVE_STREAM,
-    result_callback=result_callback
+    result_callback=result_callback,
+    output_face_blendshapes=True
 )
 
 # THE LIVE STREAM
@@ -39,6 +46,12 @@ with vision.FaceLandmarker.create_from_options(options) as landmarker:
                 py = int(point.y * h)
                 
                 cv2.circle(frame, (px, py), 1, (0, 255, 0), -1 )
+                
+                
+        if result.face_blendshapes:
+            for score in result.face_blendshapes[0]:
+                if score.category_name == "mouthSmile" and score.score > 0.5:
+                    print("You are smiling!")
             
         cv2.imshow('Face Tracker', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
